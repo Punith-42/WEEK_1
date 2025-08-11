c18bc27443aaa1d9b53c943239164d5fcfd20b45
Punith-42 <thummapunith@gmail.com>
2025-08-11 10:30:25 +0530

    user-card-commit

---
+++ usg-react/src/components/usercard.js
@@ -0,0 +1,14 @@
+// src/components/UserCard.js
+import React from "react";
+
+function UserCard(props) {
+  return (
+    <div>
+      <img src={props.image} alt={props.firstName} width="50" />
+      <h3>{props.firstName} {props.lastName}</h3>
+      <p>{props.email} | {props.phone}</p>
+    </div>
+  );
+}
+
+export default UserCard;
